1fbc3fd521b29b5bea757eb843be84bbc33998d9
Paul 'Worthless' Nijjar <pnijjar@alumni.uwaterloo.ca>
2017-06-07 11:26:17 -0400

    Made a loop

--- mail_newsletter.sh
+++ mail_newsletter.sh
@@ -1,32 +1,77 @@
 #!/bin/bash
 
 # Can I send out newsletters?
+# (Not very well)
+
+
+# ===== CONFIG/CONSTANTS 
+
+# configfile bodyfile 
+# to_addr 
+# from_addr account 
+declare -A PARAM
+PARAM[watcamp]="watcamp.config.py watcamp.txt \
+  watcamp-newsletter@kwlug.org \
+  newsletter-watcamp@kwlug.org newsletter.watcamp.kwlug.org"
+PARAM[spectrum]="spectrum.config.py spectrum.txt \
+  paul_nijjar@yahoo.ca \
+  newsletter-spectrum@kwlug.org newsletter.spectrum.kwlug.org"
 
 
 PYDIR="/home/pnijjar/google-calendar-helpers"
 CONFDIR="/home/pnijjar/gcal-confs"
+BODYDIR="/home/pnijjar/output"
+
+
+# ===== FUNCTIONS 
+
+# $1 : the newsletter ID ("spectrum", "watcamp")
+# I am doing this because I do not how to encode a
+# date in the array.
+function get_subject () { 
+  datestring=`date +"%b %d, %Y"`
+  
+  if [[ $1 == "spectrum" ]]
+  then
+    retval="Rainbow Clearinghouse Events ($datestring listing)"
+  else
+    retval="$datestring events listing"
+  fi  
+
+  # retval is now a GLOBAL VARIABLE. Ugh. 
+} 
+
+
+# ===== MAIN PROGRAM 
+
 
 source "$PYDIR/venv/bin/activate"
-$PYDIR/gen_newsletter.py --configfile $CONFDIR/watcamp.config.py
-
-WATCAMP_BODY="/home/pnijjar/output/watcamp.txt"
 
 
-email_subject=`date +"%b %d, %Y events listing"`
-email_dest="watcamp-newsletter@kwlug.org"
-email_body=$(<$WATCAMP_BODY)
+# ${!PARAM[@]} - get keys
+for newsletter in "${!PARAM[@]}"
+do
+
+    read conffile bodyfile to_addr from_addr account <<< "${PARAM[$newsletter]}"
 
 
-read -d '' EMAIL <<- EOF
-To: $email_dest
-From: newsletter-watcamp@kwlug.org
+    $PYDIR/gen_newsletter.py --configfile $CONFDIR/$conffile
+
+
+    get_subject $newsletter
+    email_subject="$retval"
+    email_body=$(<$BODYDIR/$bodyfile)
+
+
+    read -d '' EMAIL <<- EOF
+To: $to_addr
+From: $from_addr
 Subject: $email_subject 
 Content-Type: text/plain; charset=UTF-8
 
 $email_body
 EOF
 
-echo "$EMAIL" | \
-msmtp -a newsletter.watcamp.kwlug.org $email_dest
-
-
+    echo "$EMAIL" | \
+    msmtp -a $account $to_addr
+done 
